9228afad4bf2ff5e3b45f1a43657c7b0244ff9f1
OnlyHardOfficial <marcelocoutinho_1994@hotmail.com>
2018-06-18 02:51:48 +0100

    Initial commit

---
+++ components/invoice_templates/default_invoice/language/pt_pt/default_invoice.php
@@ -0,0 +1,25 @@
+<?php
+$lang['DefaultInvoice.page_of'] = "Página %1\$s de %2\$s"; // %1$s is the current page number, %2$s is the total number of pages
+$lang['DefaultInvoice.terms_heading'] = "Termos";
+$lang['DefaultInvoice.type_active'] = "FATURA";
+$lang['DefaultInvoice.type_draft'] = "FATURA - RASCUNHO";
+$lang['DefaultInvoice.type_void'] = "FATURA - ANULADA";
+$lang['DefaultInvoice.client_id_code'] = "Número de Cliente:";
+$lang['DefaultInvoice.date_billed'] = "Data da Fatura:";
+$lang['DefaultInvoice.notes_heading'] = "Notas";
+$lang['DefaultInvoice.subtotal_heading'] = "Subtotal";
+$lang['DefaultInvoice.tax_heading'] = "%1\$s (%2\$s%%)"; // %1$s is the tax name, %2$s is the tax rate, %% escapes the percent (%) sign so it can be displayed
+$lang['DefaultInvoice.total_heading'] = "Total";
+$lang['DefaultInvoice.lines_description'] = "Descrição";
+$lang['DefaultInvoice.lines_quantity'] = "Quantidade";
+$lang['DefaultInvoice.lines_unit_price'] = "Preço por unidade";
+$lang['DefaultInvoice.lines_cost'] = "Custo";
+$lang['DefaultInvoice.watermark_paid'] = "PAGO";
+$lang['DefaultInvoice.type_proforma'] = "FATURA PRO FORMA";
+$lang['DefaultInvoice.payments_heading'] = "Pagamentos";
+$lang['DefaultInvoice.payments_type_name'] = "Tipo";
+$lang['DefaultInvoice.payments_transaction_id'] = "Número de Transação";
+$lang['DefaultInvoice.payments_applied_amount'] = "Montante aplicado";
+$lang['DefaultInvoice.proforma_id_code'] = "Número Pró-forma:";
+$lang['DefaultInvoice.draft_id_code'] = "Número de Rascunho:";
+?>
